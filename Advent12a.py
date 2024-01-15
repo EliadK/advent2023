5f1e2afed7263c70d309462101d323ff365c2b6e
EliadK <eliad.kimhy@gmail.com>
2024-01-15 17:56:11 +0100

    Advent 12a

--- Advent12a.py
+++ Advent12a.py
@@ -1,53 +1,65 @@
-def starter(stage, position, comparison, input):
+def starter(stage, position, comparison):
     if stage < len(stages)-1:
         dots = 0
-        while dots <= len(input[0])-position-sum(stages[stage:])-len(stages[stage:])+1:
+        while comparison.find(stages[stage]*"#",position+dots) <= len(input[0])-(sum(stages[stage:])+len(stages[stage:])-1):
             if comparison.find(stages[stage]*"#",position+dots) > -1:
                 match_spot=comparison.find(stages[stage]*"#",position+dots)
-                if "#" in input[0][position+dots:match_spot]:
+                if "#" in input[0][position:match_spot]:
                     break
-                elif position+dots < match_spot:
-                    print("we're too far")
-                    pass
-                elif input[0][match_spot+stages[stage]]!="#":
-                    starter(stage+1,match_spot + stages[stage] + 1,comparison[:match_spot + stages[stage]] + "." + comparison[match_spot + stages[stage] + 1:],input)
+                if position+dots < match_spot:
+                    dots = match_spot - position
+                if input[0][match_spot+stages[stage]]!="#":
+                    starter(stage+1,match_spot + stages[stage] + 1,comparison[:match_spot + stages[stage]] + "." + comparison[match_spot + stages[stage] + 1:])
                 if "#" in input[0][position+dots]:
-                    print("no more room for dot")
                     break
                 else:
                     dots+=1
             else:
                 break
     if stage == len(stages)-1:
-        print("reached last stage")
-        last_starter(stage, position,comparison,input)
+        if comparison.find(stages[stage] * "#", position) == -1:
+            pass
+        else:
+            last_starter(stage, position,comparison)
 
-def last_starter(stage, position, comparison, input):
+def last_starter(stage, position, comparison):
     dots = 0
-    while dots <= len(input[0])-position-sum(stages[stage:])-len(stages[stage:])+1:
+    while comparison.find(stages[stage]*"#",position+dots) <= len(input[0])-(sum(stages[stage:])+len(stages[stage:])-1):
         if comparison.find(stages[stage]*"#",position+dots) > -1:
             match_spot = comparison.find(stages[stage] * "#", position + dots)
-            if "#" in input[0][match_spot +stages[stage]:]:
-                print("no bueno")
-                pass
-            elif position + dots < match_spot:
-                print("we're too far")
-                pass
-            elif match_spot + stages[stage] == len(comparison):
-                global count
+            global count
+            if "#" in input[0][position:match_spot]:
+                break
+            if "#" in input[0][match_spot + stages[stage]:]:
+                if "#" in input[0][position + dots]:
+                    break
+                else:
+                    dots += 1
+                    continue
+            if position + dots < match_spot:
+                dots = match_spot - position
+            if match_spot + stages[stage] == len(comparison):
                 count += 1
+            elif "#" not in input[0][match_spot:]:
+                if "." not in input[0][match_spot +stages[stage]:]:
+                    count+=len(input[0][match_spot +stages[stage]:])+1
+                else:
+                    while comparison.find(stages[stage] * "#", position + dots) <= len(input[0]) - (sum(stages[stage:]) + len(stages[stage:]) - 1) and comparison.find(stages[stage]*"#",position+dots) > -1:
+                        count+=1
+                        dots=(comparison.find(stages[stage] * "#", position + dots)+1)-position
+                break
             else:
-                # global count
                 count+=1
             if "#" in input[0][position + dots]:
-                print("no more room for dot")
                 break
             else:
                 dots+=1
         else:
             break
 
+import time
 
+t = time.process_time()
 final=0
 n=0
 with open("input12.txt", "r") as file:
@@ -57,9 +69,11 @@ with open("input12.txt", "r") as file:
         # prepare the string
         input=input.split(" ")
         input[1]=[int(item) for item in input[1].split(",")]
+        input[1]=list(reversed(input[1]))
         stages=input[1]
         #split ?
         input[0]= [item for item in input[0]]
+        input[0]=list(reversed(input[0]))
         #remove excess dots
         index=0
         while index < len(input[0])-1:
@@ -78,7 +92,8 @@ with open("input12.txt", "r") as file:
         comparison="".join(comparison) # create one big # string for easy searches
         # now we need to iterate over the comparison string, while keeping original in mind
         count=0
-        starter(0, 0, comparison, input)
+        starter(0, 0, comparison)
         print(count)
         final+=count
 print(final)
+print(time.process_time() - t)
